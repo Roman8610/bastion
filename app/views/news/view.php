07a645cba21a1fa089a204e344b38b6156f32ad1
Roman Drozdenko <roman8610@gmail.com>
2025-01-28 11:31:38 +0000

    Исправленны шрифты в новостях

--- app/views/news/view.php
+++ app/views/news/view.php
@@ -1,14 +1,8 @@
-<div class="py-16">
-    <div class="container mx-auto">
-      <h1 class="font-bold text-3xl text-gray-700 text-center"><?=$new->title?></h1>
+<div class="basis-full mt-4 lg:mt-8">
+  <article class="prose relative">
+    <h1 class="h2"><?=$new->title?></h1>
+    <div class="prose-content">
+      <?=$new->text?>
     </div>
-
-    <div class="mt-8 grid grid-cols-1 gap-16">
-      <section>
-        <div class="container mx-auto flex flex-col items-center">
-          <div class="lg:prose-xl">
-                <?=$new->text?>
-          </div>
-        </div>
-    </div>
-  </div>
+  </article>
+</div>
