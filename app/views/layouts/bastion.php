136da89615bc8c1b143c96db197fcf99094c9d18
Roman Drozdenko <roman8610@gmail.com>
2025-02-17 10:48:07 +0000

    СЕО оптимизация

--- app/views/layouts/bastion.php
+++ app/views/layouts/bastion.php
@@ -33,23 +33,6 @@ $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_k
 <html lang="<?= Yii::$app->language ?>">
 
 <head>
-  <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(99857885, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/99857885" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
   <meta charset="UTF-8">
   <meta name="viewport"
     content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -58,6 +41,9 @@ $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_k
   <!-- <link rel="stylesheet" href="assets/styles/main.min.css" /> -->
   <link rel="shortcut icon" type="image/x-icon" href="/favicon.svg">
   <?php $this->head() ?>
+  
+  <meta name="yandex-verification" content="37e3d1ea403dfa46" />
+  <meta name="google-site-verification" content="_inKbAFSpHdTfR_UIN2c_TMtqokjp1IfwUAl6Y1LTVI" />
 </head>
 
 <body x-data :class="$store.modals.isOpen ? 'overflow-hidden' : ''" class="pt-[75px] md:pt-0">
@@ -463,6 +449,23 @@ $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_k
   <script src="assets/js/manifest.js"></script>
   <script src="assets/js/vendor.min.js"></script>
   <script src="assets/js/main.min.js"></script>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+	   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	   m[i].l=1*new Date();
+	   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	   ym(99857885, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+	   });
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/99857885" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
   <?php $this->endBody() ?>
 </body>
 </html>
