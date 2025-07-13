32dcaec49fd111021c4c5e4658c80dce0fbb3136
Roman Drozdenko <roman8610@gmail.com>
2025-07-13 13:50:21 +0000

    Исключена директория appnew из гита

--- appnew/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    //'bsVersion' => '5', 
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-];
--- appnew/views/news/view.php
+++
@@ -1,8 +0,0 @@
-<div class="basis-full mt-4 lg:mt-8">
-  <article class="prose relative">
-    <h1 class="h2"><?=$new->title?></h1>
-    <div class="prose-content">
-      <?=$new->text?>
-    </div>
-  </article>
-</div>
